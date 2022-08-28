b98a7cd6719c45f92426cbf4fee13d46991430cb
llx <541165610@qq.com>
2022-08-28 12:19:49 +0800

    first commit

---
+++ src/main/java/com/xc/GitTest.java
@@ -0,0 +1,13 @@
+package com.xc;
+
+/**
+ * @Description : TODO
+ * @Author lilianxu
+ * @Date 12:18 2022/8/28
+ **/
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git ");
+    }
+}
